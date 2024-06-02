9ac90674944e35c73e8fb0bfda12b59cab1ff73f
adeariyo27 <adeharioyudanto@gmail.com>
2024-06-02 20:29:51 +0800

    Adding to project: Task 3 - Table Booking System

--- Task 3 - Table Booking System/StoredProcedure_CancelBooking.sql
+++ Task 3 - Table Booking System/StoredProcedure_CancelBooking.sql
@@ -8,7 +8,7 @@ DELIMITER //
 
 CREATE PROCEDURE CancelBooking(IN Booking_id INT)
 BEGIN
-    DELETE FROM bookings WHERE BookingID = Booking_id;
+    DELETE FROM Bookings WHERE BookingID = Booking_id;
     SELECT CONCAT("Booking ", Booking_id, " cancelled") AS "Confirmation";
 END //
 DELIMITER ;
